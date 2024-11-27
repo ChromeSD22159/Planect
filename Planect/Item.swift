36ba7aa51e33adb42177b0ee74897d36c069fcfd
Frederik Kohler <info@frederikkohler.de>
2024-11-27 16:52:11 +0100

    Initial Commit

---
+++ Planect/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Planect
+//
+//  Created by Frederik Kohler on 27.11.24.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
